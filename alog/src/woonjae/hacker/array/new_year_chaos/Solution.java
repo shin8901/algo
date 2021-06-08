2ebe514a1a9db95210e1e149926a03d7d55d336c
woonjae.shin <woonjae.shin@miracom.co.kr>
2021-06-08 18:45:55 +0900

    ddddd

--- alog/src/woonjae/hacker/array/new_year_chaos/Solution.java
+++ alog/src/woonjae/hacker/array/new_year_chaos/Solution.java
@@ -1,16 +1,8 @@
 package woonjae.hacker.array.new_year_chaos;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 class Result {
 
@@ -19,9 +11,39 @@ class Result {
 	 *
 	 * The function accepts INTEGER_ARRAY q as parameter.
 	 */
+	public static void 인출하기(List<Integer> q) {
+
+	}
 
 	public static void minimumBribes(List<Integer> q) {
 		// Write your code here
+
+		int count = 0;
+		for (int i = 0; i < q.size(); i++) {
+			if (q.get(i) - (i + 1) > 2) {
+				System.out.println("Too chaotic");
+				return;
+			}
+		}
+
+		for (int i = 0; i < q.size(); i++) {
+			if (i + 1 != q.get(i)) {
+				// same 위치
+				int tmpS = i + 1; // 원래 있어야할 값.
+				for (int j = i + 1; j < q.size(); j++) {
+					if (q.get(j) == tmpS) {
+						int tmp = q.get(j - 1);
+						q.set(j - 1, q.get(j));
+						q.set(j, tmp);
+						count += 1;
+						i--;
+						j = q.size();
+					}
+				}
+			}
+		}
+
+		System.out.println(count);
 
 	}
 
@@ -29,23 +51,26 @@ class Result {
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-		int t = Integer.parseInt(bufferedReader.readLine().trim());
-
-		IntStream.range(0, t).forEach(tItr -> {
-			try {
-				int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-				List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-						.map(Integer::parseInt).collect(toList());
-
-				Result.minimumBribes(q);
-			} catch (IOException ex) {
-				throw new RuntimeException(ex);
-			}
-		});
-
-		bufferedReader.close();
+		Integer[] array = { 1, 2, 5, 3, 4, 7, 8, 6 };
+		List<Integer> q = Arrays.asList(array);
+		Result.minimumBribes(q);
+//		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//		int t = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//		IntStream.range(0, t).forEach(tItr -> {
+//			try {
+//				int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//				List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//						.map(Integer::parseInt).collect(toList());
+//
+//				Result.minimumBribes(q);
+//			} catch (IOException ex) {
+//				throw new RuntimeException(ex);
+//			}
+//		});
+//
+//		bufferedReader.close();
 	}
 }
